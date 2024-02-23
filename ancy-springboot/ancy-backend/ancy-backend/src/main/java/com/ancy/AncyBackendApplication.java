de0b378b42aa5ca8de953ae36f6fedd62b905fb3
Ggxy159 <axyyu@outlook.com>
2024-02-23 15:56:22 +0800

    Initial commit: Set up project structure

---
+++ ancy-springboot/ancy-backend/ancy-backend/src/main/java/com/ancy/AncyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncyBackendApplication.class, args);
+    }
+
+}
---
+++ ancy-springboot/ancy-backend/ancy-backend/src/test/java/com/ancy/AncyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
