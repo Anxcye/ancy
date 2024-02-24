e8e2145d252f1119ddacd3067e6e625d9cf5995a
Ggxy159 <axyyu@outlook.com>
2024-02-24 11:08:47 +0800

    Initial commit: Set up project structure

--- ancy-vue/ancy-admin/src/router/index.ts
+++ ancy-vue/ancy-admin/src/router/index.ts
@@ -1,10 +1,7 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-Vue.use(VueRouter)
-
-const routes: Array<RouteConfig> = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
@@ -20,7 +17,8 @@ const routes: Array<RouteConfig> = [
   }
 ]
 
-const router = new VueRouter({
+const router = createRouter({
+  history: createWebHashHistory(),
   routes
 })
 
